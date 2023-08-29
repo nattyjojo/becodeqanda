27908e61ca6608e5e3efe88c51fadc44795ca3ff
prynskaf <prince.kaf12@gmail.com>
2023-08-29 09:03:41 +0200

    header updated

--- src/components/createQuiz/qustion.jsx
+++ src/components/createQuiz/qustion.jsx
@@ -1,10 +1,13 @@
 
 export default function Question(){
     return(
-        
+        <div>
+        <i className="fa-regular fa-circle text-white mt-7 ml-10 fa-2x" />
         <input className="text-center text-[#444343] mt-2 font-semibold py-[.7rem] px-[30vw]" type="text" name="qustion" id="qustion"  placeholder="Enter Your Qustion"/>
-        // <i className="fa-regular fa-circle text-white mt-7 ml-10 fa-2
-        // x">
+        <i className="fa-regular fa-circle text-white mt-7 ml-10 fa-2x" />
+        </div>
+        
+    
         // <input  className="text-center text-[#444343] pt-3"
         //  type="text" name="answer" id="answer" placeholder="Add Answer"  />
         // </i>
--- src/components/hearder/Header.jsx
+++ src/components/hearder/Header.jsx
@@ -1,20 +1,17 @@
 import React, { useEffect, useState } from "react";
+import { Link } from "react-router-dom"; // Import the Link component
 
 const Header = () => {
-  // State to manage whether the menu is shown or not
   const [showMenu, setShowMenu] = useState(false);
 
-  // Effect to update the menu visibility based on window size
   useEffect(() => {
     const handleResize = () => {
       setShowMenu(window.innerWidth <= 768);
     };
 
-    // Call once on component mount to set initial state
     handleResize();
     window.addEventListener("resize", handleResize);
 
-    // Cleanup: remove the event listener when the component unmounts
     return () => {
       window.removeEventListener("resize", handleResize);
     };
@@ -22,15 +19,13 @@ const Header = () => {
 
   return (
     <div id="header" className="bg-secondary-color bg-opacity-50 text-light-color flex justify-between align-middle p-5 text-lg font-medium">
-      <div className=" logo cursor-pointer">
-        
-          <a href="/home">
+      <div className="logo cursor-pointer">
+        <Link to="/home">
           &lt;/<span className="logo text-button-color">be</span>
           <span>codeqanda</span>&gt;
-          </a>
+        </Link>
       </div>
       {showMenu ? (
-        // Show menu button on small screens
         <button
           id="toggleButton"
           className="focus:outline-none"
@@ -38,33 +33,19 @@ const Header = () => {
             setShowMenu(!showMenu);
           }}
         >
-          <svg
-            className="h-6 w-6 text-light-color hover:text-button-color"
-            viewBox="0 0 24 24"
-            fill="none"
-            stroke="currentColor"
-            aria-hidden="true"
-          >
-            <path
-              strokeLinecap="round"
-              strokeLinejoin="round"
-              strokeWidth="2"
-              d="M4 6h16M4 12h16M4 18h16"
-            ></path>
-          </svg>
+          {/* ... (your existing code) */}
         </button>
       ) : (
-        // Show menu links on larger screens
         <div id="navbar" className="flex gap-2">
-          <a href="/Home" className="hover:text-button-color" >
+          <Link to="/home" className="hover:text-button-color">
             Home
-            </a>
-          <a href="/Register" className="hover:text-button-color">
+          </Link>
+          <Link to="/register" className="hover:text-button-color">
             Register
-          </a>
-          <a href="/Login" className="hover:text-button-color">
+          </Link>
+          <Link to="/login" className="hover:text-button-color">
             Login
-          </a>
+          </Link>
         </div>
       )}
     </div>
