6fc17aae57ce3ae5d97ce0389985e3e03f70ab7d
prynskaf <prince.kaf12@gmail.com>
2023-09-11 10:44:49 +0200

    updated files

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,11 +1,12 @@
 const Footer = () => {
   return (
-    <footer className="flex justify-center items-center bg-secondary-color bg-opacity-90 text-center text-light-color min-h-[4rem] w-[100vw]">
-    <p className="p-5 text-lg font-medium"> &copy; 2023 All Rights Reserved </p>
-  </footer>
-  
-    );
+    <footer
+      className="flex justify-center items-center bg-secondary-color bg-opacity-90 text-center text-light-color min-h-[4rem] w-[100vw] fixed bottom-0"
+      style={{ zIndex: 999 }}
+    >
+      <p className="p-5 text-lg font-medium">&copy; 2023 All Rights Reserved</p>
+    </footer>
+  );
 };
+
 export default Footer;
-
-
