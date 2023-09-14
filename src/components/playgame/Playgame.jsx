3df7d61e6978a02f752365bb019069bcb585d4eb
prynskaf <prince.kaf12@gmail.com>
2023-09-14 15:43:53 +0200

    playgame updated

--- src/components/playgame/Playgame.jsx
+++ src/components/playgame/Playgame.jsx
@@ -1,11 +1,16 @@
 import React, { useState, useEffect, useMemo } from "react";
-import { FaUser, FaStar, FaTimesCircle, FaSyncAlt ,FaBars, FaMedal } from "react-icons/fa";
+import {
+  FaUser,
+  FaStar,
+  FaTimesCircle,
+  FaSyncAlt,
+  FaBars,
+  FaMedal,
+} from "react-icons/fa";
 import "./Playgame.css";
 
-
 export default function Playgame(props) {
-  const optStyle =
-    "m-2 px-5 py-3 md:px-40 md:py-5 inline-block solid rounded text-2xl md:text-5xl";
+  const optStyle = "m-2 px-5 py-3 md:px-40 md:py-5 inline-block solid rounded text-2xl md:text-5xl";
   const questionStyle = "bg-[green] text-white p-3 rounded  mb-20";
   const answerStyle = "bg-green-300 text-green-900 p-3 rounded cursor-pointer ";
 
@@ -21,16 +26,61 @@ export default function Playgame(props) {
         options: ["Venus", "Mars", "Jupiter", "Saturn"],
         correctAnswer: "Mars",
       },
-      // ... (other quiz questions)
+      {
+        question: "What is the largest ocean in the world?",
+        options: ["Atlantic Ocean", "Indian Ocean", "Arctic Ocean", "Pacific Ocean"],
+        correctAnswer: "Pacific Ocean",
+      },
+      {
+        question: "Who painted the Mona Lisa?",
+        options: ["Leonardo da Vinci", "Pablo Picasso", "Vincent van Gogh", "Michelangelo"],
+        correctAnswer: "Leonardo da Vinci",
+      },
+      {
+        question: "Which gas do plants absorb from the atmosphere?",
+        options: ["Oxygen", "Carbon Dioxide", "Nitrogen", "Hydrogen"],
+        correctAnswer: "Carbon Dioxide",
+      },
+      {
+        question: "What is the currency of Japan?",
+        options: ["Yen", "Dollar", "Euro", "Pound"],
+        correctAnswer: "Yen",
+      },
+      {
+        question: "Which country is known as the Land of the Rising Sun?",
+        options: ["China", "South Korea", "Japan", "Thailand"],
+        correctAnswer: "Japan",
+      },
+      {
+        question: "Who wrote the play 'Romeo and Juliet'?",
+        options: ["William Shakespeare", "Charles Dickens", "Jane Austen", "Mark Twain"],
+        correctAnswer: "William Shakespeare",
+      },
+      {
+        question: "What is the largest mammal in the world?",
+        options: ["African Elephant", "Giraffe", "Blue Whale", "Lion"],
+        correctAnswer: "Blue Whale",
+      },
+      {
+        question: "Which gas makes up the majority of Earth's atmosphere?",
+        options: ["Oxygen", "Carbon Dioxide", "Nitrogen", "Hydrogen"],
+        correctAnswer: "Nitrogen",
+      },
+      
     ],
     []
   );
 
+  const COUNTDOWN_DURATION = 12;
+
   const [currentQuestionIndex, setCurrentQuestionIndex] = useState(0);
-  const [countdown, setCountdown] = useState(12);
+  const [countdown, setCountdown] = useState(COUNTDOWN_DURATION);
   const [points, setPoints] = useState(0);
   const [quizRunning, setQuizRunning] = useState(true);
   const [quizFinished, setQuizFinished] = useState(false);
+  const [incorrectResponses, setIncorrectResponses] = useState([]);
+  const [isWaiting, setIsWaiting] = useState(false);
+  const [questionAnswered, setQuestionAnswered] = useState(false);
 
   const stopQuiz = () => {
     setQuizRunning(false);
@@ -42,18 +92,23 @@ export default function Playgame(props) {
     setQuizFinished(false);
     setCurrentQuestionIndex(0);
     setPoints(0);
-    setCountdown(12);
+    setCountdown(COUNTDOWN_DURATION);
+    setIncorrectResponses([]);
+    resetAnswerStyles();
   };
 
   useEffect(() => {
     const countdownInterval = setInterval(() => {
-      if (countdown > 0 && quizRunning) {
+      if (countdown > 0 && quizRunning && !isWaiting) {
         setCountdown(countdown - 1);
-      } else {
+      } else if (quizRunning && !isWaiting) {
         clearInterval(countdownInterval);
         if (currentQuestionIndex < quizData.length - 1) {
           setCurrentQuestionIndex(currentQuestionIndex + 1);
-          setCountdown(12);
+          setCountdown(COUNTDOWN_DURATION);
+          setQuestionAnswered(false);
+          setIsWaiting(false);
+          resetAnswerStyles();
         } else {
           setQuizRunning(false);
           setQuizFinished(true);
@@ -62,49 +117,82 @@ export default function Playgame(props) {
     }, 1000);
 
     return () => clearInterval(countdownInterval);
-  }, [countdown, currentQuestionIndex, quizData, quizRunning]);
+  }, [countdown, currentQuestionIndex, quizData, quizRunning, isWaiting]);
 
   const handleAnswerClick = (selectedOption) => {
-    if (!quizRunning || quizFinished) {
-      return; // Don't handle clicks if the quiz is not running or has finished.
+    if (!quizRunning || quizFinished || isWaiting || questionAnswered) {
+      return;
     }
 
     const currentQuestion = quizData[currentQuestionIndex];
     if (selectedOption === currentQuestion.correctAnswer) {
       setPoints(points + 10);
+      setQuestionAnswered(true);
+      setIsWaiting(true);
+
+      document.getElementById(selectedOption).classList.add("correctAnswer");
+    } else {
+      setIncorrectResponses([
+        ...incorrectResponses,
+        {
+          question: currentQuestion.question,
+          selectedAnswer: selectedOption,
+          correctAnswer: currentQuestion.correctAnswer,
+        },
+      ]);
+
+      setQuestionAnswered(true);
+      setIsWaiting(true);
+
+      document.getElementById(selectedOption).classList.add("wrongAnswer");
     }
 
-    if (currentQuestionIndex < quizData.length - 1) {
-      setCurrentQuestionIndex(currentQuestionIndex + 1);
-      setCountdown(12);
-    } else {
-      setQuizRunning(false);
-      setQuizFinished(true);
-    }
+    setTimeout(() => {
+      if (currentQuestionIndex < quizData.length - 1) {
+        setCurrentQuestionIndex(currentQuestionIndex + 1);
+        setCountdown(COUNTDOWN_DURATION);
+        setQuestionAnswered(false);
+        setIsWaiting(false);
+        resetAnswerStyles();
+      } else {
+        setQuizRunning(false);
+        setQuizFinished(true);
+      }
+    }, 3000);
   };
 
   const currentQuestion = quizData[currentQuestionIndex];
 
   const [isUserProfileVisible, setIsUserProfileVisible] = useState(true);
 
-  // Function to toggle the visibility of the user profile
   const toggleUserProfile = () => {
     setIsUserProfileVisible(!isUserProfileVisible);
   };
 
-  // CSS class for the user profile section with animations
   const userProfileClass = `userProfile p-6 bg-white md:w-2/5 mb-8 md:ml-auto rounded-md shadow-md ${
     isUserProfileVisible ? "animate-slide-in" : "animate-slide-out"
   }`;
 
+  const resetAnswerStyles = () => {
+    const answerElements = document.getElementsByClassName("answer");
+    for (let i = 0; i < answerElements.length; i++) {
+      answerElements[i].classList.remove("correctAnswer", "wrongAnswer");
+      console.log(i);
+    }
+  };
+
   return (
-    <main className="flex flex-col md:flex-row flex-shrink-0"
-    style={{
-      backgroundImage: `url(${props.backgroundImg})`,
-      backgroundRepeat: 'no-repeat',
-      backgroundSize: 'cover'
-    }}
-  >
+    // main content 
+    <main
+      className="flex flex-col md:flex-row "
+      style={{
+        backgroundImage: `url(${props.backgroundImg})`,
+        backgroundRepeat: "no-repeat",
+        backgroundSize: "cover",
+      }}
+    >    
+    
+     {/* Quiz content  */}
       <section className="quizContent text-white text-center p-1 rounded-md pt-2 md:w-5/5 bg-cover bg-center md:h-screen">
         <div className="counter flex justify-between">
           <div className="countNumber text-white flex items-center justify-center rounded-full bg-[black] w-14 h-14 md:w-20 md:h-20 font-bold text-xl md:text-2xl mt-2 md:mt-4 ml-4">
@@ -124,11 +212,12 @@ export default function Playgame(props) {
             <h1 className={`${optStyle} ${questionStyle}`}>
               {currentQuestion.question}
             </h1>
-            <div className="flex flex-wrap justify-center">
+            <div className="flex flex-wrap justify-center items-center flex-row">
               {currentQuestion.options.map((option, index) => (
                 <span
                   key={index}
-                  className={`${optStyle} ${answerStyle} mt-4 mx-2`}
+                  id={option}
+                  className={`${optStyle} ${answerStyle} flex  mt-4 mx-2 answer w-50`}
                   onClick={() => handleAnswerClick(option)}
                 >
                   {option}
@@ -138,62 +227,83 @@ export default function Playgame(props) {
           </div>
         )}
         {!quizRunning && quizFinished && (
-          <div className="mt-8 md:mt-20">
+          <div className="mt-8 md:mt-20 flex justify-center items-center">
             <h1 className={`${optStyle} ${questionStyle}`}>Quiz Finished!</h1>
-            {/* You can display the user's final score or any other post-quiz information here. */}
           </div>
         )}
       </section>
-
+       
+       {/* user profile */}
       <section className={userProfileClass}>
-  <div className="userProfileHeader mb-4">
-    <button
-      className="bg-transparent border-none outline-none cursor-pointer"
-      onClick={toggleUserProfile}
-    >
-      <FaBars className="text-blue-500 text-2xl" />
-    </button>
-  </div>
-  <h1 className="userNickName text-xl md:text-2xl font-semibold mb-2">
-    Player Name
-  </h1>
-  <div className="userStats">
-    <p className="userPoints text-xl md:text-2xl font-semibold">
-      Points: {points} <FaStar className="ml-2 text-yellow-400" />
-    </p>
-    <p className="userRank text-lg md:text-xl">
-      Rank: 3rd <FaMedal className="ml-1 text-gray-500" />
-    </p>
-  </div>
-  <div className="userButtons mt-4">
-    <button
-      className="bg-red-500 text-white px-4 py-2 rounded mr-2 hover:bg-red-600"
-      onClick={stopQuiz}
-    >
-      <FaTimesCircle className="mr-1" /> Stop Quiz
-    </button>
-    <button
-      className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
-      onClick={resetQuiz}
-    >
-      <FaSyncAlt className="mr-1" /> Reset Quiz
-    </button>
-  </div>
-  <div className="userProfileContent mt-4 text-sm md:text-base">
-    <p className="mb-2">
-      <FaUser className="text-blue-500 mr-2" /> Welcome to the quiz game! Test your knowledge and earn points to climb the leaderboard.
-    </p>
-    <p className="mb-2">
-      <FaUser className="text-blue-500 mr-2" /> Keep answering questions correctly to boost your rank and compete with other players.
-    </p>
-    <p className="mb-2">
-      <FaUser className="text-blue-500 mr-2" /> Explore various categories and challenges to become a quiz champion.
-    </p>
-    <p>
-      <FaUser className="text-blue-500 mr-2" /> Don't forget to share your achievements with friends and challenge them to beat your score!
-    </p>
-  </div>
-</section>
+        <div className="userProfileHeader mb-4">
+          <button
+            className="bg-transparent border-none outline-none cursor-pointer"
+            onClick={toggleUserProfile}
+          >
+            <FaBars className="text-blue-500 text-2xl" />
+          </button>
+        </div>
+        <h1 className="userNickName text-xl md:text-2xl font-semibold mb-2">
+          Player Name
+        </h1>
+        <div className="userStats">
+          <p className="userPoints text-xl md:text-2xl font-semibold">
+            Points: {points} <FaStar className="ml-2 text-yellow-400" />
+          </p>
+          <p className="userRank text-lg md:text-xl">
+            Rank: 3rd <FaMedal className="ml-1 text-gray-500" />
+          </p>
+        </div>
+        <div className="userButtons mt-4">
+          <button
+            className="bg-red-500 text-white px-4 py-2 rounded mr-2 hover:bg-red-600"
+            onClick={stopQuiz}
+          >
+            <FaTimesCircle className="mr-1" /> Stop Quiz
+          </button>
+          <button
+            className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+            onClick={resetQuiz}
+          >
+            <FaSyncAlt className="mr-1" /> Reset Quiz
+          </button>
+        </div>
+        <div className="userProfileContent mt-4 text-sm md:text-base bg-[#dddcdc69]">
+          <p className="mb-2">
+            <FaUser className="text-blue-500 mr-2" /> Welcome to the quiz game!
+            Test your knowledge and earn points to climb the leaderboard.
+          </p>
+          <p className="mb-2">
+            <FaUser className="text-blue-500 mr-2" /> Keep answering questions
+            correctly to boost your rank and compete with other players.
+          </p>
+          <p className="mb-2">
+            <FaUser className="text-blue-500 mr-2" /> Explore various categories
+            and challenges to become a quiz champion.
+          </p>
+          <p>
+            <FaUser className="text-blue-500 mr-2" /> Dont forget to share your
+            achievements with friends and challenge them to beat your score!
+          </p>
+        </div>
+        <div className="trackQuestions">
+          <h2 className="text-lg md:text-xl font-semibold mb-2">
+            Incorrect Responses
+          </h2>
+          <ul className="bg-[#dddcdc69]">
+            {incorrectResponses.map((item, index) => (
+              <li key={index} className="mb-1">
+                <p className="text-red-500 font-semibold">{item.question}:</p>
+                <p>
+                  Your Answer: {item.selectedAnswer}
+                  <br />
+                  Correct Answer: {item.correctAnswer}
+                </p>
+              </li>
+            ))}
+          </ul>
+        </div>
+      </section>
     </main>
   );
 }
