d73d423885e5015ec0ddd97a9588debba074c1e6
prynskaf <prince.kaf12@gmail.com>
2023-09-14 08:53:53 +0200

    files uodated

--- src/components/playgame/Playgame.jsx
+++ src/components/playgame/Playgame.jsx
@@ -1,4 +1,7 @@
 import React, { useState, useEffect, useMemo } from "react";
+import { FaUser, FaStar, FaTimesCircle, FaSyncAlt ,FaBars, FaMedal } from "react-icons/fa";
+import "./Playgame.css";
+
 
 export default function Playgame(props) {
   const optStyle =
@@ -18,61 +21,7 @@ export default function Playgame(props) {
         options: ["Venus", "Mars", "Jupiter", "Saturn"],
         correctAnswer: "Mars",
       },
-      {
-        question: "What is the largest ocean in the world?",
-        options: [
-          "Atlantic Ocean",
-          "Indian Ocean",
-          "Arctic Ocean",
-          "Pacific Ocean",
-        ],
-        correctAnswer: "Pacific Ocean",
-      },
-      {
-        question: "Who painted the Mona Lisa?",
-        options: [
-          "Leonardo da Vinci",
-          "Pablo Picasso",
-          "Vincent van Gogh",
-          "Michelangelo",
-        ],
-        correctAnswer: "Leonardo da Vinci",
-      },
-      {
-        question: "Which gas do plants absorb from the atmosphere?",
-        options: ["Oxygen", "Carbon Dioxide", "Nitrogen", "Hydrogen"],
-        correctAnswer: "Carbon Dioxide",
-      },
-      {
-        question: "What is the currency of Japan?",
-        options: ["Yen", "Dollar", "Euro", "Pound"],
-        correctAnswer: "Yen",
-      },
-      {
-        question: "Which country is known as the Land of the Rising Sun?",
-        options: ["China", "South Korea", "Japan", "Thailand"],
-        correctAnswer: "Japan",
-      },
-      {
-        question: "Who wrote the play 'Romeo and Juliet'?",
-        options: [
-          "William Shakespeare",
-          "Charles Dickens",
-          "Jane Austen",
-          "Mark Twain",
-        ],
-        correctAnswer: "William Shakespeare",
-      },
-      {
-        question: "What is the largest mammal in the world?",
-        options: ["African Elephant", "Giraffe", "Blue Whale", "Lion"],
-        correctAnswer: "Blue Whale",
-      },
-      {
-        question: "Which gas makes up the majority of Earth's atmosphere?",
-        options: ["Oxygen", "Carbon Dioxide", "Nitrogen", "Hydrogen"],
-        correctAnswer: "Nitrogen",
-      },
+      // ... (other quiz questions)
     ],
     []
   );
@@ -136,8 +85,26 @@ export default function Playgame(props) {
 
   const currentQuestion = quizData[currentQuestionIndex];
 
+  const [isUserProfileVisible, setIsUserProfileVisible] = useState(true);
+
+  // Function to toggle the visibility of the user profile
+  const toggleUserProfile = () => {
+    setIsUserProfileVisible(!isUserProfileVisible);
+  };
+
+  // CSS class for the user profile section with animations
+  const userProfileClass = `userProfile p-6 bg-white md:w-2/5 mb-8 md:ml-auto rounded-md shadow-md ${
+    isUserProfileVisible ? "animate-slide-in" : "animate-slide-out"
+  }`;
+
   return (
-    <main className="flex flex-col md:flex-row flex-shrink-0">
+    <main className="flex flex-col md:flex-row flex-shrink-0"
+    style={{
+      backgroundImage: `url(${props.backgroundImg})`,
+      backgroundRepeat: 'no-repeat',
+      backgroundSize: 'cover'
+    }}
+  >
       <section className="quizContent text-white text-center p-1 rounded-md pt-2 md:w-5/5 bg-cover bg-center md:h-screen">
         <div className="counter flex justify-between">
           <div className="countNumber text-white flex items-center justify-center rounded-full bg-[black] w-14 h-14 md:w-20 md:h-20 font-bold text-xl md:text-2xl mt-2 md:mt-4 ml-4">
@@ -178,36 +145,55 @@ export default function Playgame(props) {
         )}
       </section>
 
-      <section className="userProfile p-6 bg-white md:w-2/5 mb-8">
-        <h1 className="userNickName text-xl md:text-2xl">Player Name</h1>
-        <p className="userPoints text-xl md:text-2xl">Points: {points}</p>
-        <p className="userRank text-lg md:text-xl">Rank: 3rd</p>
-        <p className="mt-4 text-sm md:text-base">
-          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed
-          tincidunt, arcu a ultrices rhoncus, tortor tellus iaculis arcu, nec
-          eleifend dolor orci vel ante. Quisque quis orci vel urna euismod
-          tristique. Integer convallis elementum quam, et laoreet erat facilisis
-          ut.
-        </p>
-        <div className="mt-4 ">
-          {quizRunning && !quizFinished && (
-            <button
-              className="bg-red-500 text-white px-4 py-2 rounded mr-2"
-              onClick={stopQuiz}
-            >
-              Stop Quiz
-            </button>
-          )}
-          {!quizRunning && (
-            <button
-              className="bg-blue-500 text-white px-4 py-2 rounded mr-2"
-              onClick={resetQuiz}
-            >
-              Reset Quiz
-            </button>
-          )}
-        </div>
-      </section>
+      <section className={userProfileClass}>
+  <div className="userProfileHeader mb-4">
+    <button
+      className="bg-transparent border-none outline-none cursor-pointer"
+      onClick={toggleUserProfile}
+    >
+      <FaBars className="text-blue-500 text-2xl" />
+    </button>
+  </div>
+  <h1 className="userNickName text-xl md:text-2xl font-semibold mb-2">
+    Player Name
+  </h1>
+  <div className="userStats">
+    <p className="userPoints text-xl md:text-2xl font-semibold">
+      Points: {points} <FaStar className="ml-2 text-yellow-400" />
+    </p>
+    <p className="userRank text-lg md:text-xl">
+      Rank: 3rd <FaMedal className="ml-1 text-gray-500" />
+    </p>
+  </div>
+  <div className="userButtons mt-4">
+    <button
+      className="bg-red-500 text-white px-4 py-2 rounded mr-2 hover:bg-red-600"
+      onClick={stopQuiz}
+    >
+      <FaTimesCircle className="mr-1" /> Stop Quiz
+    </button>
+    <button
+      className="bg-blue-500 text-white px-4 py-2 rounded hover:bg-blue-600"
+      onClick={resetQuiz}
+    >
+      <FaSyncAlt className="mr-1" /> Reset Quiz
+    </button>
+  </div>
+  <div className="userProfileContent mt-4 text-sm md:text-base">
+    <p className="mb-2">
+      <FaUser className="text-blue-500 mr-2" /> Welcome to the quiz game! Test your knowledge and earn points to climb the leaderboard.
+    </p>
+    <p className="mb-2">
+      <FaUser className="text-blue-500 mr-2" /> Keep answering questions correctly to boost your rank and compete with other players.
+    </p>
+    <p className="mb-2">
+      <FaUser className="text-blue-500 mr-2" /> Explore various categories and challenges to become a quiz champion.
+    </p>
+    <p>
+      <FaUser className="text-blue-500 mr-2" /> Don't forget to share your achievements with friends and challenge them to beat your score!
+    </p>
+  </div>
+</section>
     </main>
   );
 }
