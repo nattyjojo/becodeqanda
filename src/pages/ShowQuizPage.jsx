e6293fe2ef6d311241804c1618748ff9fc661900
prynskaf <prince.kaf12@gmail.com>
2023-09-15 17:30:42 +0200

    showgamepage

---
+++ src/pages/ShowQuizPage.jsx
@@ -0,0 +1,14 @@
+import Header from '../components/hearder/Header';
+import Footer from '../components/footer/footer';
+import ShowQuiz from '../components/showQuiz/ShowQuiz';
+
+
+export default function ShowQuizPage() {
+  return (
+    <div>
+       <Header />
+       <ShowQuiz />
+      <Footer />
+    </div>
+  )
+}
